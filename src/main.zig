541a9d9af075cb36583999737efb0be58888dc95
Quin Gillespie <trypsynth@gmail.com>
2025-12-23 21:57:14 -0700

    Make the config dir and file, move to a Config struct.

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,25 @@
 const std = @import("std");
 
+const Config = struct {
+    fn load(allocator: std.mem.Allocator) !void {
+        const data_dir = try std.fs.getAppDataDir(allocator, "ped");
+        defer allocator.free(data_dir);
+        std.fs.makeDirAbsolute(data_dir) catch |err| switch (err) {
+            error.PathAlreadyExists => {},
+            else => return err,
+        };
+        const config_path = try std.fs.path.join(allocator, &[_][]const u8{ data_dir, "config.json" });
+        defer allocator.free(config_path);
+        const file = try std.fs.cwd().createFile(config_path, .{});
+        defer file.close();
+    }
+};
+
 const Pet = struct {
     name: []const u8,
 };
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    const data_dir = try std.fs.getAppDataDir(allocator, "ped");
-    defer allocator.free(data_dir);
-    const config_path = try std.fs.path.join(allocator, &[_][]const u8{ data_dir, "config.json" });
-    std.debug.print("{s}\n", .{config_path});
+    try Config.load(allocator);
 }
