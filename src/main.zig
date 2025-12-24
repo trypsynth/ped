f65fbc8424594de21b39333684a3d418e8385208
Quin Gillespie <trypsynth@gmail.com>
2025-12-24 10:44:22 -0700

    Cleanup imports.

--- src/main.zig
+++ src/main.zig
@@ -1,29 +1,25 @@
 const std = @import("std");
-const debug = std.debug;
-const fs = std.fs;
-const heap = std.heap;
-const mem = std.mem;
 
 const Config = struct {
-    fn load(allocator: mem.Allocator) !void {
+    fn load(allocator: std.mem.Allocator) !void {
         const config_path = try getConfigPath(allocator);
         defer allocator.free(config_path);
-        const file = try fs.cwd().createFile(config_path, .{});
+        const file = try std.fs.cwd().createFile(config_path, .{});
         defer file.close();
         var file_buf: [256]u8 = undefined;
         var reader = file.reader(&file_buf);
         const content = try reader.interface.allocRemaining(allocator, .unlimited);
-        debug.print("got {s}\n", .{content});
+        std.debug.print("got {s}\n", .{content});
     }
 
-    fn getConfigPath(allocator: mem.Allocator) ![]const u8 {
-        const data_dir = try fs.getAppDataDir(allocator, "ped");
+    fn getConfigPath(allocator: std.mem.Allocator) ![]const u8 {
+        const data_dir = try std.fs.getAppDataDir(allocator, "ped");
         defer allocator.free(data_dir);
-        fs.makeDirAbsolute(data_dir) catch |err| switch (err) {
+        std.fs.makeDirAbsolute(data_dir) catch |err| switch (err) {
             error.PathAlreadyExists => {},
             else => return err,
         };
-        const path = try fs.path.join(allocator, &[_][]const u8{ data_dir, "config.json" });
+        const path = try std.fs.path.join(allocator, &[_][]const u8{ data_dir, "config.json" });
         return path;
     }
 };
@@ -33,6 +29,6 @@ const Pet = struct {
 };
 
 pub fn main() !void {
-    const allocator = heap.page_allocator;
+    const allocator = std.heap.page_allocator;
     try Config.load(allocator);
 }
