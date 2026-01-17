619c9c8d44a02d7546e340ec1fe546a1da170093
Quin Gillespie <trypsynth@gmail.com>
2026-01-16 18:34:37 -0700

    Implement super basic JSON saving

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 )
 
 func main() {
 	name, err := promptForName()
 	if err != nil {
+		log.Fatal(err)
+	}
+	if err := savePet(name); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(name)
@@ -24,4 +30,26 @@ func promptForName() (string, error) {
 		return "", err
 	}
 	return strings.TrimSpace(line), nil
+}
+
+type petState struct {
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func savePet(name string) error {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	state := petState{
+		Name:      name,
+		CreatedAt: time.Now().UTC(),
+	}
+	data, err := json.MarshalIndent(state, "", "\t")
+	if err != nil {
+		return err
+	}
+	statePath := filepath.Join(homeDir, ".ped.json")
+	return os.WriteFile(statePath, append(data, '\n'), 0644)
 }
