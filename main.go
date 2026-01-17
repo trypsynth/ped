6f578a5d70d9c17a21455c3f4693a2743219192f
Quin Gillespie <trypsynth@gmail.com>
2026-01-16 18:36:49 -0700

    Load the name on startup

--- main.go
+++ main.go
@@ -11,17 +11,6 @@ import (
 	"time"
 )
 
-func main() {
-	name, err := promptForName()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := savePet(name); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(name)
-}
-
 func promptForName() (string, error) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Name: ")
@@ -37,19 +26,61 @@ type petState struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-func savePet(name string) error {
-	homeDir, err := os.UserHomeDir()
+func loadPet() (petState, error) {
+	statePath, err := petStatePath()
 	if err != nil {
-		return err
+		return petState{}, err
 	}
+	data, err := os.ReadFile(statePath)
+	if err != nil {
+		return petState{}, err
+	}
+	var state petState
+	if err := json.Unmarshal(data, &state); err != nil {
+		return petState{}, err
+	}
+	return state, nil
+}
+
+func savePet(name string) error {
 	state := petState{
 		Name:      name,
 		CreatedAt: time.Now().UTC(),
 	}
-	data, err := json.MarshalIndent(state, "", "\t")
+	data, err := json.MarshalIndent(state, "", "  ")
 	if err != nil {
 		return err
 	}
-	statePath := filepath.Join(homeDir, ".ped.json")
+	statePath, err := petStatePath()
+	if err != nil {
+		return err
+	}
 	return os.WriteFile(statePath, append(data, '\n'), 0644)
+}
+
+func petStatePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(homeDir, ".ped.json"), nil
+}
+
+func main() {
+	state, err := loadPet()
+	if err == nil && state.Name != "" {
+		fmt.Println(state.Name)
+		return
+	}
+	if err != nil && !os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+	name, err := promptForName()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := savePet(name); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(name)
 }
